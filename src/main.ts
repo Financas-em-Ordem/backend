d5c060cc3035e4c46cfab0bdb104b64181cebd10
Augusto <42296932+4august@users.noreply.github.com>
2024-03-08 00:22:44 -0400

    VAI

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOptions: CorsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: corsOptions});
 
-  const allowCors = fn => async (req, res) => {
+  /*const allowCors = fn => async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
     // another common pattern
@@ -23,7 +31,9 @@ async function bootstrap() {
   }
   
 
-  app.use(allowCors)
+  app.use(allowCors)*/
+
+ 
 
   app.useGlobalPipes(new ValidationPipe());
 
